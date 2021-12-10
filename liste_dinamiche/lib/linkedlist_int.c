303655c8dbf2e8fe6f46c02e33ee37092ac3ddfb
eumonti <eugeniomontinaro@gmail.com>
2021-12-10 11:07:32 +0100

    separate linkedlist libraries

---
+++ liste_dinamiche/lib/linkedlist_int.c
@@ -0,0 +1,73 @@
+#include "linkedlist_int.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void printList(Node *list)
+{
+  while (list != NULL)
+  {
+    printf("%d ", list->data);
+    list = list->next;
+  }
+  printf("\n");
+}
+
+Node *newNode(int data)
+{
+  Node *new = malloc(sizeof(Node));
+  if (new != NULL)
+  {
+    new->next = NULL;
+    new->data = data;
+  }
+  return new;
+}
+
+Node *getSequence(char *fileName)
+{
+  FILE *filePtr;
+
+  Node *head = NULL;
+  Node *curr, *last;
+  int first = 1;
+
+  if ((filePtr = fopen(fileName, "r")) == NULL)
+    return NULL;
+
+  fseek(filePtr, 0, SEEK_END);
+  unsigned long len = (unsigned long)ftell(filePtr);
+  if (len == 0)
+    return NULL;
+  rewind(filePtr);
+  while (!feof(filePtr))
+  {
+    int data;
+    fscanf(filePtr, "%d", &data);
+    curr = newNode(data);
+    if (first)
+    {
+      head = curr;
+      first = 0;
+    }
+    else
+    {
+      last->next = curr;
+    }
+    last = curr;
+  }
+  curr->next = NULL;
+
+  fclose(filePtr);
+  return head;
+}
+
+void freeList(Node *list)
+{
+  Node *nextNode;
+  while (list != NULL)
+  {
+    nextNode = list->next;
+    free(list);
+    list = nextNode;
+  }
+}
