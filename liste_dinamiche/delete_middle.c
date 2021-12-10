3c1d741b55a63f404eece00c329145d0662b9e7f
Eugenio <eugeniomontinaro@gmail.com>
2021-12-10 14:35:42 +0100

    create delete_middle.c

--- liste_dinamiche/delete_middle.c
+++ liste_dinamiche/delete_middle.c
@@ -2,7 +2,13 @@
 #include <stdlib.h>
 #include "lib/linkedlist.h"
 
-void deleteMiddle(Node **pList)
+typedef enum
+{
+  false,
+  true
+} bool;
+
+void deleteMiddleOld(Node **pList)
 {
   Node *tmp;
   Node *slow, *fast;
@@ -44,10 +50,98 @@ void deleteMiddle(Node **pList)
   free(tmp);
 }
 
+// void deleteMiddle2(Node **pList)
+// {
+// Node **pSlow, *fast;
+// int moveSlow = 0;
+// if (*pList == NULL)
+//   return;
+
+// fast = (*pList)->next;
+// *pSlow = pList;
+
+// while (fast != NULL)
+// {
+//   if (moveSlow)
+//   {
+//     printf("Incrementing slow\n");
+//     *pSlow = (*pSlow)->next;
+//   }
+//   moveSlow = !moveSlow;
+
+//   fast = fast->next;
+// }
+
+// tmp = pSlow->next;
+// pSlow->next = pSlow->next->next;
+// free(tmp);
+// }
+
+void deleteMiddle(Node **pList)
+{
+  Node *pCurr, *pNodeToDel;
+  Node *pTmp;
+  bool moveSlow = false;
+
+  if (*pList = NULL)
+    return;
+
+  pCurr = (*pList)->next;
+  pNodeToDel = *pList;
+  while (pCurr != NULL)
+  {
+    if (moveSlow)
+      pNodeToDel = pNodeToDel->next;
+
+    moveSlow = !moveSlow;
+    pCurr = pCurr->next;
+  }
+
+  // Delete node
+  // pTmp = pNodeToDel->next;
+  // pNodeToDel->next = pNodeToDel->next->next;
+  pTmp = pNodeToDel;
+  pNodeToDel = pNodeToDel->next;
+  free(pTmp);
+}
+
+void deleteMiddleFontana(Node **pList)
+{
+  Node *curr;
+
+  Node **pNodeToDel;
+  Node *nodeToDel;
+
+  bool moveSlow;
+
+  if (NULL == *pList)
+    return;
+
+  curr = (*pList)->next;
+  pNodeToDel = pList;
+
+  moveSlow = false;
+
+  while (NULL != curr)
+  {
+    if (moveSlow)
+      pNodeToDel = &((*pNodeToDel)->next);
+
+    moveSlow = !moveSlow;
+    curr = curr->next;
+  }
+
+  /* eliminazione dell'elemento centrale della lista */
+  nodeToDel = *pNodeToDel;
+  *pNodeToDel = nodeToDel->next;
+  free(nodeToDel);
+}
 int main()
 {
   Node *list = getSequence("sequence1.txt");
   printList(list);
-  deleteMiddle(&list);
+  // deleteMiddle2(&list);
+  // deleteMiddleFontana(&list);
+  deleteMiddleFontana(&list);
   printList(list);
 }
