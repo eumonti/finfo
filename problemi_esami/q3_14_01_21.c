814b6604e3cfdde23e0836bd2291c27101426e1c
Eugenio <eugeniomontinaro@gmail.com>
2022-02-14 18:53:02 +0100

    more efficient approach

--- problemi_esami/q3_14_01_21.c
+++ problemi_esami/q3_14_01_21.c
@@ -89,7 +89,7 @@ int recursiveFun(char *string)
     if (br == 0)
     {
       printf("Sequenza ripetuta: ");
-      for(i = 0; i < P; i++)
+      for (i = 0; i < P; i++)
         printf("%c", string[i]);
       printf("\n");
       return P;
@@ -99,10 +99,87 @@ int recursiveFun(char *string)
   return recursiveFun(string + 1);
 }
 
+int caglioti(char *string)
+{
+  int len;
+  int trovato;
+  int T;
+  int cont;
+  int max;
+  int i;
+  char *punt;
+
+  for (len = 0; string[len] != '\0'; len++)
+    ;
+
+  trovato = 0;
+  T = len / 2;
+  while (T > 0 && trovato == 0)
+  {
+    cont = 0;
+    max = cont;
+    for (i = 0; i < len - T; i++)
+      if (string[i + T] == string[i])
+      {
+        cont++;
+        if (cont > max)
+        {
+          max = cont;
+          punt = string + i - max + 1;
+        }
+      }
+      else
+        cont = 0;
+    if (max >= T)
+      trovato = 1;
+    else
+      T--;
+  }
+}
+
+int TrovaPeriodo(char *string)
+{
+  int len, periodo;
+  int i;
+  int inizio;
+  int br;
+
+  len = 0;
+  while (string[len] != '\0')
+    len++;
+
+  for (periodo = 1; periodo < len / 2; periodo++)
+  {
+    inizio = -1;
+    br = 0;
+    for (i = 0; i < len - periodo && !br; i++)
+    {
+      if (string[i] == string[i + periodo])
+      {
+        if (inizio == -1)
+          inizio = i;
+      }
+      else if (inizio != -1)
+      {
+        br = 1;
+      }
+    }
+
+    if (!br && inizio != -1)
+    {
+      printf("Periodo: ");
+      printString(string + inizio, periodo);
+      return periodo;
+    }
+  }
+  printf("Non periodica\n");
+  return 0;
+}
+
 int main()
 {
   char *string = "quipartepausapartepar";
   // int result = fun(string);
-  int result = recursiveFun(string);
+  int result = TrovaPeriodo(string);
   result ? printf("Stringa periodica con P=%d.\n", result) : printf("Stringa non periodica.\n");
 }
